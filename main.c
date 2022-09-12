6931720a5ce36f0e72f6150720b17c969e6afdf2
Kshitij A <kshitij.everest@gmail.com>
2022-09-12 10:32:30 +0530

    gitignore and input.txt file added

--- main.c
+++ main.c
@@ -14,7 +14,9 @@ int randBitFlip(int m, int k){
 }
 
 int crcSender(char* sgp, char* data){
-
+    if(checkError()==0){
+        
+    }
 }
 
 int crcReceiver(char* sgp, char* data){
@@ -25,14 +27,26 @@ int main(){
     // read n characters from a file at a time
     int n = 10;
     char* data = (char*)malloc(n*sizeof(char));
-    FILE* fp = fopen("input.txt", "r");
-    int i = 0;
-    while(i<n){
-        fscanf(fp, "%c", &data[i]);
-        i++;
+    char* sgf = "110110101";
+    
+    FILE *fp2;
+    char ch;
+    fp2 = fopen("input.txt","r");
+    ch = fgetc(fp2);
+    while(ch != EOF)
+    {
+        int i=0;
+        while(ch!=EOF && i<n){
+            data[i] = ch;
+            ch = fgetc(fp2);
+            i++;
+        }
+        crcSender(sgf,data);
+        //check for error
+            //if no error
+                //continue for next n char
+            //if error
     }
-    fclose(fp);
-    // crcSender(sgp, data);
-    // crcReceiver(sgp, data);
+    fclose(fp2);
     return 0;
 }
