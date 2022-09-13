cf87ca692a42ecf8823cc10316077ab4faa83b20
Abhitator <89402434+Abhitator216@users.noreply.github.com>
2022-09-13 12:40:08 +0530

    mod2div implementation

--- main.c
+++ main.c
@@ -1,78 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+char* substr(const char *src, int m, int n)
+{
+	// get the length of the destination string
+	int len = n - m;
 
-char* xor(char* a, char* b) {
+	// allocate (len + 1) chars for destination (+1 for extra null character)
+	char *dest = (char*)malloc(sizeof(char) * (len + 1));
+
+	// extracts characters between m'th and n'th index from source string
+	// and copy them into the destination string
+	for (int i = m; i < n && (*(src + i) != '\0'); i++)
+	{
+		*dest = *(src + i);
+		dest++;
+	}
+
+	// null-terminate the destination string
+	*dest = '\0';
+
+	// return the destination string
+	return dest - len;
+}
+char *xor1(char *a, char *b)
+{
     int i;
-    char* result = malloc(strlen(a) + 1);
-    for (i = 0; i < strlen(a); i++) {
+    char *result = malloc(strlen(a) + 1);
+    for (i = 0; i < strlen(a); i++)
+    {
         result[i] = a[i] ^ b[i];
     }
     result[i] = '\0';
     return result;
 }
+char* mod2div(char* divd, char* divs)
+{
+    int sz=sizeof(divd)/sizeof(divd[0]);
+    int len=sizeof(divs)/sizeof(divs[0]);
+    int i=0,j=0;
+    char *rem;
+    while(i<sz){
+        char* nstr=substr(divd,i,i+len);
+        rem=xor1(nstr,divs);
+        i=i+len;
 
-// perform modulo-2 division and return the remainder in binary
-char* mod2div(char* dividend, char* divisor) {
-    int pick = strlen(divisor);
-    int tmp = pick;
-    char* remainder = malloc(strlen(divisor) + 1);
-    //todo
-    return remainder;
+        while(rem[j]!='1'){
+            j++;
+        }
+        strcat(rem,substr(divd,i,i+j+1));
+    }
+    char* ret = "laodao";
+    return ret;
 }
 
-char* stringToBinary(char* s) {
-    if(s == NULL) return 0; /* no input string */
+char *stringToBinary(char *s)
+{
+    if (s == NULL)
+        return 0; /* no input char* */
     size_t len = strlen(s);
-    char *binary = malloc(len*8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
+    char *binary = malloc(len * 8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
     binary[0] = '\0';
-    for(size_t i = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i)
+    {
         char ch = s[i];
-        for(int j = 7; j >= 0; --j){
-            if(ch & (1 << j)) {
-                strcat(binary,"1");
-            } else {
-                strcat(binary,"0");
+        for (int j = 7; j >= 0; --j)
+        {
+            if (ch & (1 << j))
+            {
+                strcat(binary, "1");
+            }
+            else
+            {
+                strcat(binary, "0");
             }
         }
     }
     return binary;
 }
 
-int checkError(){
+int checkError()
+{
     int r1 = rand();
-    return r1%2; 
+    return r1 % 2;
 }
 
-int randBitFlip(int m, int k){
+int randBitFlip(int m, int k)
+{
     int r2 = rand();
-    int i = r2%(m+k);
+    int i = r2 % (m + k);
     return i;
 }
 
-char* crcCalculator(char* data, char* sgp){
-    char* dataWithZeros = malloc(strlen(data) + strlen(sgp) + 1);
+char *crcCalculator(char *data, char *sgp)
+{
+    char *dataWithZeros = malloc(strlen(data) + strlen(sgp) - 1);
     strcpy(dataWithZeros, data);
     int i;
-    for(i = 0; i < strlen(sgp) - 1; i++){
+    for (i = 0; i < strlen(sgp) - 1; i++)
+    {
         strcat(dataWithZeros, "0");
     }
-    char* remainder = mod2div(dataWithZeros, sgp);
+    char *remainder = mod2div(dataWithZeros, sgp);
     printf("\nRemainder: %s", remainder);
-    char* crc = malloc(strlen(data) + strlen(remainder) + 1);
+    char *crc = malloc(strlen(data) + strlen(remainder) + 1);
     strcpy(crc, data);
     strcat(crc, remainder);
     return crc;
 }
 
-int crcReceiver(char*,char*,char*);
+int crcReceiver(char *, char *, char *);
 
-void crcSender(char* sgp, char* data){
-    //convert data to binary string
-    char* binData = stringToBinary(data);
-    
-    //calculate crc and append to fullBinData
-    char* crcBin = crcCalculator(data, sgp);
+void crcSender(char *sgp, char *data)
+{
+    // convert data to binary char*
+    char *binData = stringToBinary(data);
+
+    // calculate crc and append to fullBinData
+    char *crcBin = crcCalculator(binData, sgp);
     printf("\ncrc gen: %s", crcBin);
 
     // if(checkError()){
@@ -88,19 +133,20 @@ void crcSender(char* sgp, char* data){
     return;
 }
 
-int crcReceiver(char* sgp, char* data, char* fullBinData){
-    
+int crcReceiver(char *sgp, char *data, char *fullBinData)
+{
 }
 
-int main(){
+int main()
+{
     // read n characters from a file at a time
     int n = 10;
-    char* data = (char*)malloc(n*sizeof(char));
-    char* sgf = "110110101";
-    
+    char *data = (char *)malloc(n * sizeof(char));
+    char *sgf = "110110101";
+
     FILE *fp2;
     char ch;
-    fp2 = fopen("input.txt","r");
+    fp2 = fopen("input.txt", "r");
     ch = fgetc(fp2);
     // while(ch != EOF)
     // {
@@ -116,16 +162,17 @@ int main(){
     //             //continue for next n char
     //         //if error
     // }
-    int i=0;
-    while(ch!=EOF && i<n){
-            data[i] = ch;
-            ch = fgetc(fp2);
-            i++;
-        }
+    int i = 0;
+    while (ch != EOF && i < n)
+    {
+        data[i] = ch;
+        ch = fgetc(fp2);
+        i++;
+    }
     fclose(fp2);
     // for(int i=0;i<n;i++){
     //     printf("%c",data[i]);
     // }
-    crcSender(sgf,data);
+    crcSender(sgf, data);
     return 0;
 }
